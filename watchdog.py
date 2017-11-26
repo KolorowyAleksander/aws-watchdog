998e81e60411ffab04390b9b768ecaddb1df12b6
KolorowyAleksander <KolorowyAleksander@users.noreply.github.com>
2017-11-27 00:49:33 +0100

    Add config fetching

--- watchdog.py
+++ watchdog.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python3
+import argparse
+
 import boto3
+from botocore.exceptions import EndpointConnectionError
+
+# constants
+TABLE_NAME = 'aszymanski-watchdog-table'
+
+DESCRIPTION = 'Watchdog for checking services statuses'
+ID_HELP = 'Id of the DynamoDB row with required configuration'
 
 
 def main():
-    print(boto3.__version__)
+    parser = argparse.ArgumentParser(prog='watchdog', description=DESCRIPTION)
+    parser.add_argument('id', help=ID_HELP)
+    args = parser.parse_args()
+    
+    try:
+        get_config(args.id)
+    except KeyError:  # the configuration entry doesn't exist
+        pass
+
+
+def get_config(id: int):
+    """Requests a row from db with the required config."""
+    ddb = boto3.resource('dynamodb')
+    table = ddb.Table(TABLE_NAME)
+
+    try:
+        res = table.get_item(Key={'id': id})
+    except EndpointConnectionError:  # cannot connect to the DB
+        pass
 
 
 if __name__ == '__main__':
     main()
+
