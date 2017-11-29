27186e2acccd943602142c112f2d79143b08f740
KolorowyAleksander <KolorowyAleksander@users.noreply.github.com>
2017-11-30 00:58:51 +0100

    Add proper logging, change method names
    Add loggers and log file handler
    Functions are now properly arranged

--- watchdog.py
+++ watchdog.py
@@ -3,22 +3,21 @@ import argparse
 import subprocess
 import time
 import threading
+import logging
 
 import boto3
 from botocore.exceptions import EndpointConnectionError
 
 # constants
 TABLE_NAME = 'aszymanski-watchdog-table'
-
-DESCRIPTION = 'Watchdog for checking services statuses'
-ID_HELP = 'Id of the DynamoDB row with required configuration'
+LOG_FILE = '/var/log/aws-watchdog.log'
 
 
 class ConfigFetcher:
     def __init__(self, id: str, table_name: str):
         """Returns up to date config, lazily
         fetching from DB when older than 15min (when get_config is called).
-        
+
         Params:
             id(str): id of the db row with requested config
             table_name(str): name of the table with requested config
@@ -33,7 +32,7 @@ class ConfigFetcher:
         response = self._table.get_item(Key={'id': self._id})
         self._config = response['Item']
         self._lastUpdated = time.time()
-        
+
     def get_config(self) -> dict:
         """Requests a row from db with the required config.
         Raises:
@@ -47,18 +46,11 @@ class ConfigFetcher:
         return self._config
 
 
-def main():
-    parser = argparse.ArgumentParser(prog='watchdog', description=DESCRIPTION)
-    parser.add_argument('id', help=ID_HELP)
-    args = parser.parse_args()
-   
-    fetcher = ConfigFetcher(args.id, TABLE_NAME)
+def run_daemon(fetcher: ConfigFetcher):
+    logger = logging.getLogger('aws-watchdog')
 
-    check_for_config(fetcher)  # exits if config is invalid
-
-    # this part should be ran as a daemon
     running_threads = dict()
-    while True:   
+    while True:
         config = fetcher.get_config()
 
         iteration_time = 10 #config['numOfSecCheck']
@@ -74,7 +66,7 @@ def main():
             if (not run_service_command(service_name, 'status')
                 and service_name not in running_threads):
                 # service down and not being restarted
-                print(f'{service_name} is down')
+                logger.info(f'{service_name} is down')
                 
                 child = threading.Thread(target=restart_service_with_retries,
                                          args=(service_name,
@@ -83,11 +75,41 @@ def main():
                 child.start()
                 running_threads[service_name] = child
             elif service_name in running_threads:
-                print(f'{service_name} is being restarted')
+                logger.info(f'{service_name} is being restarted')
             else:
-                print(f'{service_name} is running')
+                logger.info(f'{service_name} is running')
 
         time.sleep(iteration_time)
+
+
+def restart_service_with_retries(service_name: str,
+                                 retries: int,
+                                 wait_time: int):
+    logger = logging.getLogger('aws-watchdog')
+    for tries in range(1, retries+1):
+        if run_service_command(service_name, 'restart'):
+            logger.info(f'{service_name} restarted after {tries} times')
+            return
+        elif tries != retries:  # try again
+            time.sleep(wait_time)
+        else:
+            logger.info(f'{service_name} failed after {retries} restarts')
+
+
+def run_service_command(service_name: str, cmd: str):
+    """Run a shell command to check services.
+    Returns:
+        True: if the command succeded (0 return code)
+        False: if the command didn't succeed or the shell call broke.
+    """
+    # This assumes the services are running under systemctl
+    try:
+        res = subprocess.run(['systemctl', cmd, service_name],
+                             stdout=subprocess.DEVNULL,
+                             stderr=subprocess.DEVNULL)
+        return True if res.returncode == 0 else False
+    except subprocess.SubprocessError:
+        return False
 
 
 def check_for_config(fetcher: ConfigFetcher):
@@ -107,34 +129,30 @@ def check_for_config(fetcher: ConfigFetcher):
         exit(1)
 
 
-def run_service_command(service_name: str, cmd: str):
-    """Run a shell command to check services.
-    Returns:
-        True: if the command succeded (0 return code)
-        False: if the command didn't succeed or the shell call broke.
-    """
-    # This assumes the services are running under systemctl
-    try:
-        res = subprocess.run(['systemctl', cmd, service_name],
-                             stdout=subprocess.DEVNULL,
-                             stderr=subprocess.DEVNULL)
-        return True if res.returncode == 0 else False
-    except subprocess.SubprocessError:
-        return False
+def main():
+    # parse arguments
+    description = 'Watchdog for checking services statuses'
+    id_help = 'Id of the DynamoDB row with required configuration'
+    parser = argparse.ArgumentParser(prog='watchdog', description=description)
+    parser.add_argument('id', help=id_help)
+    args = parser.parse_args()
+  
+    # initiate logging 
+    logger = logging.getLogger('aws-watchdog')
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler(LOG_FILE)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
+    fetcher = ConfigFetcher(args.id, TABLE_NAME)
 
-def restart_service_with_retries(service_name: str, 
-                                 retries: int,
-                                 wait_time: int):
-    for tries in range(retries):
-        if run_service_command(service_name, 'restart'):
-            print(f'the retry for {service_name} succeded after {tries+1} times')
-            return
-        elif tries != retries:  # try again
-            time.sleep(wait_time) 
-        else:
-            print(f'could not restart {service_name} after {retries} times')
+    # exits if config is invalid
+    # this would not work like that if started for example from init.d
+    check_for_config(fetcher)
 
+    # this part should be ran as a daemon
+    run_daemon(fetcher)
 
 if __name__ == '__main__':
     main()
